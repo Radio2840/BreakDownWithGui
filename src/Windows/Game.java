60f89b5c786d61c1bf81f0bc4c3b5a51bbb1a988
kinga <kinga.smolarek1@gmail.com>
2023-09-14 12:10:46 +0200

    Added image to plank

--- src/Windows/Game.java
+++ src/Windows/Game.java
@@ -6,42 +6,60 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.Objects;
+
+
 
 public class Game implements KeyListener {
     JFrame gameframe=new JFrame("Game");
-    JLabel plank=new JLabel();
+
+    JLabel plank = new JLabel();
+
+
 
     Game() {
+        gameframe.setLayout(null);
+        plank.setBounds(290, 380, 50, 5);
+        try {
+            BufferedImage image = ImageIO.read(new File("src/Images/plankDemo.png"));
+            ImageIcon icon = new ImageIcon(image);
+            plank.setIcon(icon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        plank.setBounds(320,340,90,10);
-        plank.setBackground(Color.orange);
-        plank.setOpaque(true);
+        //plank.setBackground(Color.orange);
+       // plank.setOpaque(true);
+        //plank.setBounds(0,0,100,100);
         gameframe.addKeyListener(this);
         gameframe.setSize(640,480);
         gameframe.add(plank);
         gameframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameframe.setLayout(null);
         gameframe.setVisible(true);
         gameframe.getContentPane().setBackground(Color.black);
     }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
         int x = plank.getX();
         int y = plank.getY();
-        switch(e.getKeyChar()) {
-            case 'a':
-                plank.setLocation(x-6,y);
-                break;
-            case 'd':
-                plank.setLocation(x+6,y);
-                break;
-            default:
-                // code block
-        }
+            switch(e.getKeyChar()) {
+                case 'a':
+                    plank.setLocation(x-6,y);
+                    break;
+                case 'd':
+                    plank.setLocation(x+6,y);
+                    break;
+                default:
+                    // code block
+            }
+
     }
 
     @Override
